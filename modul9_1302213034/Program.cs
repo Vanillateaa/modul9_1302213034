38409d6a052d967987bf8abe59d4468afc9c57a6
Vanillateaa <mohamad.william24@gmail.com>
2023-04-26 16:32:16 +0700

    MENAMBAHKAN API MOVIES

--- modul9_1302213034/Program.cs
+++ modul9_1302213034/Program.cs
@@ -1,8 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+// Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,9 +16,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
